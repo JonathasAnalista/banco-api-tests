a8b09ff2a42faa4290039414ef298668118631dd
Jonathas Guilherme <jonathasguilhermeti@gmail.com>
2025-07-04 11:44:10 -0300

    Adicionei testes para GET transferências total e individual

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -17,11 +17,7 @@ const postTransferencias = require('../fixtures/postTransferencias.json');
 
 // Inicia o grupo de testes relacionado à funcionalidade de transferências
 describe('Transferências', () => {
-
-  // Subgrupo específico para os testes da rota POST /transferencias
-  describe('POST /transferencias', () => {
-
-    // Declara uma variável que irá armazenar o token de autenticação antes de cada teste
+  // Declara uma variável que irá armazenar o token de autenticação antes de cada teste
     let token;
 
     // Executa antes de cada teste individual (it)
@@ -29,6 +25,9 @@ describe('Transferências', () => {
     beforeEach(async () => {
       token = await obterToken('julio.lima', '123456'); // Faz login com credenciais válidas e guarda o token
     });
+
+  // Subgrupo específico para os testes da rota POST /transferencias
+  describe('POST /transferencias', () => {
 
     // Teste 1: Verifica se a API retorna status 201 quando o valor da transferência for válido (>= 10)
     it('Deve retornar sucesso com 201 quando o valor de transferência for igual ou acima de R$ 10,00', async () => {
@@ -67,5 +66,38 @@ describe('Transferências', () => {
     });
 
   }); // Fim do grupo de testes da rota POST /transferencias
+
+  describe('GET /transferencias/{id}', () => {
+      it('Deve retornar sucesso com 200 e dados iguais ao registro de transferência contido no banco de dados quando o ID for válido', async () => {
+            const resposta = await request(process.env.BASE_URL)
+            .get('/transferencias/12')
+            .set('Authorization', `Bearer ${token}`)
+
+        console.log(resposta.status)
+        console.log(resposta.body)
+        expect(resposta.status).to.equal(200)
+        expect(resposta.body.id).to.equal(12)
+        expect(resposta.body.id).to.be.a('number')
+        expect(resposta.body.conta_origem_id).to.equal(1)
+        expect(resposta.body.conta_destino_id).to.equal(2)
+        expect(resposta.body.valor).to.equal(11.00)
+
+
+      
+    })
+
+  })
+
+  describe('GET /transferencias', () => {
+        it('Deve retornar 10 elementos na paginacao quando informar limite de 10 registros', async () => {
+           const resposta = await request(process.env.BASE_URL)
+              .get('/transferencias?page=1&limit=10')
+              .set('Authorization', `Bearer ${token}`)
+
+           expect(resposta.status).to.equal(200)
+           expect(resposta.body.limit).to.equal(10)
+           expect(resposta.body.transferencias).to.have.lengthOf(10)
+        })
+  })
 
 }); // Fim do grupo principal de testes Transferências
